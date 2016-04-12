6f0026fc78183356f4bbbeaf3be59e0c2d8cf8c5
Nallefjant <bobby_kristensen@hotmail.com>
2016-04-12 20:27:00 +0200

    Swedish Truth

---
+++ SwedishFramework/SwedishBoolean.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace SwedishFramework
+{
+    public class SwedishBoolean
+    {
+        public enum Status
+        {
+            Sant,
+            Falskt,
+            Ibland,
+            Kanske
+        }
+        
+        private Status _status;
+
+        private SwedishBoolean() { }
+
+        public SwedishBoolean(Status status)
+        {
+            _status = status;
+        }
+
+        public bool GetAnswer()
+        {
+            switch (_status)
+            {
+                case Status.Sant:
+                    return true;
+                case Status.Falskt:
+                    return false;
+                case Status.Ibland:
+                    if (new Random().Next(1, 100) >= 90)
+                        return true;
+                    else
+                        return false;
+                case Status.Kanske:
+                    if (new Random().Next(1, 100) == 100)
+                        return true;
+                    else
+                        return false;
+            }
+            throw new InvalidEnumArgumentException();
+        }
+    }
+}
---
+++ SwedishFrameworkTests/SwedishBooleanTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SwedishFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SwedishFrameworkTests;
+
+namespace SwedishFramework.Tests
+{
+    [TestClass()]
+    public class SwedishBooleanTests
+    {
+        [TestMethod()]
+        public void GetAnswerTest_Sant()
+        {
+            var answer = new SwedishBoolean(SwedishBoolean.Status.Sant).GetAnswer();
+
+            Assert.IsTrue(answer);
+        }
+
+        [TestMethod()]
+        public void GetAnswerTest_Falskt()
+        {
+            var answer = new SwedishBoolean(SwedishBoolean.Status.Falskt).GetAnswer();
+
+            Assert.IsFalse(answer);
+        }
+
+        [TestMethod()]
+        public void GetAnswerTest_Ibland()
+        {
+            var result = false;
+            for(int i = 0; i < 100000; i++)
+            {
+                if (new SwedishBoolean(SwedishBoolean.Status.Ibland).GetAnswer())
+                    result = true;
+            }
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void GetAnswerTest_Kanske()
+        {
+            if (new SwedishBoolean(SwedishBoolean.Status.Kanske).GetAnswer())
+                throw new KanskeActuallyMeansNoException();
+
+            Assert.IsFalse(false);
+        }
+    }
+}
